e8bb981cbab4652dab59d717a5bb0aec29d734a7
renusonaditya <renusonaditya@gmail.com>
2023-02-17 11:49:24 +0530

    20230217-1149 added styled component and removed scss styles file

--- src/components/Button.component.tsx
+++ src/components/Button.component.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
-import classes from './Button.module.scss';
+
+import styled from 'styled-components';
 
 interface IButtonComponentProps {
   cb: (event: React.MouseEvent<HTMLButtonElement>) => void;
   label: string;
 }
 
+const DefaultButton = styled.button`
+  background-color: black;
+  color: white;
+  border: 1px solid green;
+  padding: 10px;
+  & span {
+    background-color: green;
+    color: black;
+    padding: 10px;
+  }
+`;
+
 const ButtonComponent: React.FC<IButtonComponentProps> = (props: IButtonComponentProps) => {
   const { cb, label } = props;
 
   return (
-    <button className={classes.button} onClick={cb}>
+    <DefaultButton onClick={cb}>
       <span>{label}</span>
-    </button>
+    </DefaultButton>
   );
 };
 
